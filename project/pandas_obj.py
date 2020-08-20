56251a9f4eb15f2b01121fffa8517c902920fa49
Ron G <ronald.greeff@gmail.com>
2020-08-20 18:34:31 +0100

    two dicts generated - next validation of dict 2 before merging with dict1

--- project/pandas_obj.py
+++ project/pandas_obj.py
@@ -39,16 +39,18 @@ class DataObj:
         # might as well do it during db > df after querying for everything
         # data = self.fetch_page()
 
-        columns = ['netloc', 'url', 'page_height', 'page_width',
-        'text', 'top', 'left', 'width', 'height', 'label'
-        ]
+        # columns = ['netloc', 'url', 'page_height', 'page_width',
+        # 'text', 'top', 'left', 'width', 'height', 'label'
+        # ]
+        # ndf = {k: [] for k in columns}
+
         css_keys = [i[0] for i in self.db.q(""" SELECT key from csskey """)]
-        ndf = df(columns=(columns+css_keys))
 
         page_id = 1
         q = """
         SELECT site.netloc, page.url, page.window_innerHeight, page.window_innerWidth,
-        block.text, block.top, block.left, block.width, block.height, block.label,
+        block.id, block.text, block.top, block.left,
+        block.width, block.height, block.label,
         csskey.key, computed.val
         FROM site
         JOIN page ON page.site_id == site.id
@@ -57,23 +59,87 @@ class DataObj:
         JOIN csskey ON csskey.id == computed.key_id
         WHERE page.id == {id}
         """.format(id=page_id)
-        data = pd.read_sql(q, self.db.con())
+        # data = pd.read_sql(q, self.db.con())
+        # print(data.columns)
 
-        for i, row in data.iterrows():
-            print(
-                row['netloc'],
-                row['url'],
-                row['window_innerHeight'],
-                row['window_innerWidth'],
-                row['text'],
-                row['top'],
-                row['left'],
-                row['width'],
-                row['height'],
-                row['label'],
-                row['key'],
-                row['val'],
-                )
+        data = self.db.q(q)
+
+        d = {}
+        temp = {}
+        for c, row in enumerate(data):
+            block_id = row[4]
+            d[ block_id ] = {
+                # 'block_id': block_id,
+                'netloc': row[0],
+                'url': row[1],
+                'page_height': row[2],
+                'page_width': row[3],
+                'text': row[5],
+                'top': row[6],
+                'left': row[7],
+                'width': row[8],
+                'height': row[9],
+                'label': row[10],
+            }
+            if not temp.get(block_id):
+                temp[block_id] = {}
+
+            temp[block_id][row[11]] = row[12]
+
+        print([len(temp[i]) for i in temp.keys()])
+        print(temp.keys())
+        print(d.keys())
+        # ndf = {'block_id': int(),'netloc': [],'url': [], 'page_height': [],
+        # 'page_width': [],'text': [],'top': [],'width': [],'height': [],
+        # 'label': [],}
+
+        # process:
+        # d1 = {"block_id": [v,...], ... | "css_key": [v, ...]}
+        # d2 = {"block_id": {csskey: v}} -> d3 = {"block_id": {csskey: [validated_list]}}
+        # d1['block_id'][ d3['block_id'] ] = d3['block_id']['csskey']
+
+        # temp = {}
+        # for i, row in data.iterrows():
+        #
+        #     # ndf['block_id'].append(row['block_id'])
+        #     ndf['netloc'].append(row['netloc'])
+        #     ndf['url'].append(row['url'])
+        #     ndf['page_height'].append(row['window_innerHeight'])
+        #     ndf['page_width'].append(row['window_innerWidth'])
+        #     ndf['text'].append(row['text'])
+        #     ndf['top'].append(row['top'])
+        #     ndf['width'].append(row['left'])
+        #     ndf['height'].append(row['height'])
+        #     ndf['label'].append(row['label'])
+        #
+        #     block_id = row['id']
+        #     if not temp.get(block_id):
+        #         temp[block_id] = {}
+        #
+        #     temp[block_id][row['key']] = row['val']
+        #
+        #     # if not temp[i]:
+        #     #     temp[i] = {}
+        #     #     if not temp[i][row['key']]:
+        #     #         temp[i][row['key']]
+        #     #
+        #     # try:
+        #     #     temp[i][row['key']] = row['val']
+        #     # except KeyError:
+        #     #     try:
+        #     #         temp[i] = {}
+        #     #         temp[i][row['key']] += row['val']
+        #     #     except KeyError:
+        #     #         temp[i][row['key']] = {}
+        #     #         temp[i][row['key']] += row['val']
+        #
+        #
+        # print(temp[0])
+        #
+        # print(len(ndf['netloc']))
+        # print(len(ndf['label']))
+
+
 
     def show_all(self):
         with pd.option_context(
