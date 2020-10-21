504d7ecf4ec31f44b2b78c04b0d27d72e170026a
Ron G <ronald.greeff@gmail.com>
2020-10-21 12:25:28 +0100

    ordered structure and made names better

---
+++ project/database/models.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+from peewee import *
+
+class BaseModel(Model):
+
+    class Meta:
+        database = SqliteDatabase(database)
+
+class Site(BaseModel):
+    netloc = CharField(null=False, unique=True)
+
+class Page(BaseModel):
+    site = ForeignKeyField(Site, backref='records', on_delete='CASCADE')
+    url = CharField(null=False, unique=True)
+    visited = BooleanField(default=False)
+    created = DateTimeField(default=datetime.now())
+    updated = DateTimeField(default=datetime.now())
+    label = CharField(null=True)
+    is_sale = BooleanField(null=True)
+    window_innerHeight = IntegerField()
+    window_innerWidth = IntegerField()
+    defaults = CharField()
+
+class Block(BaseModel):
+    page = ForeignKeyField(Page, on_delete='CASCADE')
+    text = CharField()
+    html = CharField()
+    label = CharField(null=True)
+    path = CharField()
+    width = DecimalField()
+    height = DecimalField()
+    left = DecimalField()
+    top = DecimalField()
+
+class CSSKey(BaseModel):
+    key = CharField(unique=True)
+
+class Computed(BaseModel):
+    block = ForeignKeyField(Block, on_delete='CASCADE')
+    key = ForeignKeyField(CSSKey)
+    val = CharField(null=True)
